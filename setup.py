f6bc5d82e8e5639e89857ac52ab7f536f9f3d56e
aellien <a.r.j.ellien@uva.nl>
2021-06-16 10:08:14 +0200

    add setup & init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dawis',
+      version='0.0.1',
+      description='Detection Algorithm with Wavelet for Intracluster light Studies',
+      url='https://github.com/aellien/dawis.git',
+      author='Amaël Ellien',
+      author_email='a.r.j.ellien@uva.nl',
+      license='GNU General Public License v3.0',
+      packages=['dawis'],
+      zip_safe=False)
